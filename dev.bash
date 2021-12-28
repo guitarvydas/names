2801eb5fc9654f145a647fb4d5e50eb736841815
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-28 18:16:44 -0500

    added names.pl

--- dev.bash
+++ dev.bash
@@ -1,3 +1,3 @@
 ~/app/make.bash
 cwd=`pwd`
-prep "." "$" names.ohm names.glue --stop=1 --support=${cwd}/support.js --PREFIX="${cwd}/" <names.md
+prep "." "$" names.ohm names.glue --stop=1 --support=${cwd}/support.js <names.md
--- support.js
+++ support.js
@@ -1,7 +1,12 @@
-exports.prefix = function () {
-    if (global.argv.prefix) {
-	return global.argv.prefix;
-    } else {
-	return "";
-    }
+const fs = require ('fs');
+
+let argv;
+
+exports.setArgv = function (x) { argv = x; }
+
+exports.readTextFile = function (s) {
+    let fname = s + ".pl";
+    console.error (fname);
+    let contents = fs.readFileSync (fname, "utf-8");
+    return contents;
 }
